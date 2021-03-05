a8f84cd61a7b7789a939634eb7a5e553051c4257
Gaurav Singh <gaurav.719singh@gmail.com>
2021-03-05 14:32:01 +0530

    Update mysong.c
    Tune replaced with Birthday tune.
    Previous tune did not seem to be very pleasant.

--- mysong.c
+++ mysong.c
@@ -1,17 +1,60 @@
 #include <windows.h>
 
-void main()
-
+// Driver Code
+int main()
 {
-	Beep(262, 1000);
-	Beep(362, 1000);
-	Beep(432, 1000);
-	Beep(262, 1000);
-	Beep(542, 1000);
-	Beep(282, 1000);
-	Beep(742, 1000);
-	Beep(382, 1000);
-	Beep(262, 1000);
-	Beep(552, 1000);
-	Beep(262, 1000);
+    //Happy Birthday to you tune
+            Sleep(1000);
+            Beep(264, 125);
+            Sleep(250);
+            Beep(264, 125);
+            Sleep(125);
+            Beep(297, 500);
+            Sleep(125);
+            Beep(264, 500);
+            Sleep(125);
+            Beep(352, 500);
+            Sleep(125);
+            Beep(330, 1000);
+            Sleep(250);
+            Beep(264, 125);
+            Sleep(250);
+            Beep(264, 125);
+            Sleep(125);
+            Beep(297, 500);
+            Sleep(125);
+            Beep(264, 500);
+            Sleep(125);
+            Beep(396, 500);
+            Sleep(125);
+            Beep(352, 1000);
+            Sleep(250);
+            Beep(264, 125);
+            Sleep(250);
+            Beep(264, 125);
+            Sleep(125);
+            Beep(2642, 500);
+            Sleep(125);
+            Beep(440, 500);
+            Sleep(125);
+            Beep(352, 250);
+            Sleep(125);
+            Beep(352, 125);
+            Sleep(125);
+            Beep(330, 500);
+            Sleep(125);
+            Beep(297, 1000);
+            Sleep(250);
+            Beep(466, 125);
+            Sleep(250);
+            Beep(466, 125);
+            Sleep(125);
+            Beep(440, 500);
+            Sleep(125);
+            Beep(352, 500);
+            Sleep(125);
+            Beep(396, 500);
+            Sleep(125);
+            Beep(352, 1000);
+    return 0;
 }
